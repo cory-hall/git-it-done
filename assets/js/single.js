8129ea1afd3bd18dc947f23cf1056ea446bf4ba6
Cory Hall <ComebackCory1@gmail.com>
2022-03-08 17:53:50 -0600

    added single repo issue functionality

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,21 +1,25 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
 
-var getRepoIssues = function(repo) {
+var getRepoIssues = function (repo) {
    var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
 
-   fetch(apiUrl).then(function(response) {
+   fetch(apiUrl).then(function (response) {
       // request was successful
       if (response.ok) {
-         response.json().then(function(data) {
+         response.json().then(function (data) {
             displayIssues(data);
+
+            // check if api has paginated issues
+            displayWarning(repo);
          });
       } else {
          alert("There was a problem with your request!");
       }
    });
-};
+}
 
-var displayIssues = function(issues) {
+var displayIssues = function (issues) {
    if (issues.length === 0) {
       issueContainerEl.textContent = "This repo has no open issues!";
       return;
@@ -51,4 +55,17 @@ var displayIssues = function(issues) {
    }
 };
 
-getRepoIssues("cory-hall/run-buddy");
+var displayWarning = function(repo) {
+   // add text to warning container
+   limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+   var linkEl = document.createElement("a");
+   linkEl.textContent = "See more issues on GitHub.com";
+   linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+   linkEl.setAttribute("target", "_blank");
+
+   // append to warning container
+   limitWarningEl.appendChild(linkEl);
+}
+
+getRepoIssues("facebook/react");
